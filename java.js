694448d5c054a1b72f5fd67f87c1dd23c8322efc
Kingayotml <122526969+Kingayotml@users.noreply.github.com>
2023-02-20 10:02:57 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,22 @@
+let challenge = '30 Days Of JavaScript '
+console.log(challenge.substr(0,2))
+console.log(challenge.substr(3,21))
+console.log(challenge.includes('string'))
+console.log(challenge.split(0,21))
+console.log(challenge.replace('JavaScript','Python'))
+console.log(challenge.charAt(15))
+console.log(challenge.charCodeAt('J'))
+console.log(challenge.indexOf('a'))
+console.log(challenge.lastIndexOf('a'))
+let a = 'You cannot end a sentence with because because because  is a conjuction'
+console.log(a.indexOf('because'))
+console.log(a.lastIndexOf('because'))
+console.log(challenge.trim(''))
+console.log(challenge.endsWith(''))
+let b = '30 days of'
+let c = 'Javascript'
+console.log(b.concat(c))
+console.log(challenge.repeat(2))
+
+
+
